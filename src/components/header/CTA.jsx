bed4a98f120b240a4883e60a28a8c9853f138411
ahmedamine12 <ahmedamine.bouricha@etu.fstm.ac.ma>
2022-12-01 04:00:31 +0100

    hope is the last update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/CV Bouricha Ahmed Amine.pdf'
 const CTA = () => {
   return (
     <div className='cta'> 
-    <a href={CV} download className='btn' >Download CV </a>
+    <a href={CV} download className='btn newone' >Download CV </a>
     <a href="#contact" className='btn btn-primary'> Let's Talk</a>
     
     </div>
