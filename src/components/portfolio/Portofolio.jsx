cc9524132520207cedab417fced4cfb66694e9f4
ahmedamine12 <ahmedamine.bouricha@etu.fstm.ac.ma>
2023-03-04 20:07:54 +0100

    new commit

--- src/components/portfolio/Portofolio.jsx
+++ src/components/portfolio/Portofolio.jsx
@@ -19,8 +19,8 @@ const Portofolio = () => {
 
               <h2>Generation of dynamic restaurant websites </h2>
               <p>
-              Web Application Functionality: Dynamically create restaurant chain-related websites and it offers different functionalities such as 
-              menu management, integration of foreign services like “Square Up” and other options.
+                Web Application Functionality: Dynamically create restaurant chain-related websites and it offers different functionalities such as
+                menu management, integration of foreign services like “Square Up” and other options.
               </p>
               <h5> Angular -  Laravel  - Mysql -  UML</h5>
 
@@ -44,10 +44,10 @@ const Portofolio = () => {
 
               <h2>Covid19 Test</h2>
               <p>
-              Desktop App Functionality: Allowing to give the possibility of the presence of the Covid-19 disease 
-              based on the user's symptoms, this treatment respects a system expert integration, as well as it
-               allows you to view your latest tests without forgetting the possibility of downloading them.
-               It informs us about the health status (number of contaminants, deaths, and heals) of each city.
+                Desktop App Functionality: Allowing to give the possibility of the presence of the Covid-19 disease
+                based on the user's symptoms, this treatment respects a system expert integration, as well as it
+                allows you to view your latest tests without forgetting the possibility of downloading them.
+                It informs us about the health status (number of contaminants, deaths, and heals) of each city.
               </p>
               <h5>Java - Swing Drools - MongoDB   </h5>
               <div className='iconegit'>
@@ -64,8 +64,8 @@ const Portofolio = () => {
 
               <h2>Detection of colored objects and their direction </h2>
               <p>
-              Allows the detection of the object which has the color specified by the user as he determines 
-              the direction of the object when it is moving</p>
+                Allows the detection of the object which has the color specified by the user as he determines
+                the direction of the object when it is moving</p>
               <h5> Python with the use of this modules:argparse - cv2 - imutils - time - VideoStream - numpy</h5>
               <div className='iconegit'><a href="https://github.com/ahmedamine12/detection-of-colors-and-directions" target="__blank"> <FaGithub /></a></div>
 
@@ -101,6 +101,19 @@ const Portofolio = () => {
 
       <h2>Other Noteworthy Projects</h2>
       <ul className='UlListOtherProject'>
+        <li className='LiListOtherProject'>
+          <div className='icone'> <BiFolder /> </div>
+          <div className='iconegit'><a href="https://github.com/ahmedamine12/Import-Export-RED-Project" target="__blank"> <FaGithub /></a></div>
+          <h2>Import-Export-RED</h2>
+          <p>
+            An import and export management application within the respect of
+            the RED (economic regimes in customs) which contains a programming of
+            emails according to this regime using the "Quartz" framework as well as
+            a passage of data to an excel database, for the benefit of VINCI ENERGIES MOROCCO
+          </p>
+          <h5> Spring-boot - React - MySQL </h5>
+
+        </li>
         <li className='LiListOtherProject'>
           <div className='icone'> <BiFolder /> </div>
           <div className='iconegit'><a href="https://github.com/ahmedamine12/controlling-mousse" target="__blank"> <FaGithub /></a></div>
@@ -145,7 +158,7 @@ const Portofolio = () => {
             <a href="https://github.com/ahmedamine12/AppBanqueForm" target="__blank"> <FaGithub /></a></div>
           <h2>Performing banking transactions</h2>
           <p>
-          This project allows us to perform various banking transactions such as withdrawals and deposit with the possibility of changing
+            This project allows us to perform various banking transactions such as withdrawals and deposit with the possibility of changing
             currency and the customer can view their account information
           </p>
           <h5> C#  -  SqlServer</h5>
@@ -157,23 +170,13 @@ const Portofolio = () => {
             <a href="https://github.com/ahmedamine12/Recrutement-app-" target="__blank"> <FaGithub /></a></div>
           <h2>Recruitment management</h2>
           <p>
-          A web application that is based on the operation of mega servlet its objective is to manage the various postulations of the candidates and
+            A web application that is based on the operation of mega servlet its objective is to manage the various postulations of the candidates and
             to be able to manage them to send them a response afterward
           </p>
           <h5> Java EE - Jsp</h5>
 
         </li>
-        <li className='LiListOtherProject'>
-          <div className='icone'> <BiFolder /> </div>
-          <div className='iconegit'><a href="https://github.com/ahmedamine12/" target="__blank"> <FaGithub /></a></div>
-          <h2>Application of reports</h2>
-          <p>
-          It is an application that has the final goal of creating reports using the information of companies entered by the user without
-            forgetting that there is a separate management of the companies.
-          </p>
-          <h5> Pl/sql - Oracle Data base (using Oracle Forms and Reports software)</h5>
 
-        </li>
       </ul>
 
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,8 +3,8 @@ import './skills.css'
 import { FiDatabase } from 'react-icons/fi'
 import { VscOutput } from 'react-icons/vsc'
 import { BiCodeAlt } from 'react-icons/bi'
-import {SiDiagramsdotnet } from 'react-icons/si'
-import {MdVerified } from 'react-icons/md'
+import { SiDiagramsdotnet } from 'react-icons/si'
+import { MdVerified } from 'react-icons/md'
 
 
 const Skills = () => {
@@ -19,54 +19,55 @@ const Skills = () => {
                         <h3> Programming languages</h3>
                         <h4>
                             <ol>
-                               <li value=""> <MdVerified/> C/C++</li>
-                                <li value=""> <MdVerified/> Java</li>
-                                <li value=""><MdVerified/> JavaScript</li>
-                                <li value=""><MdVerified/> Python</li>
-                                <li value=""> <MdVerified/> php</li>
+                                <li value=""> <MdVerified /> C/C++</li>
+                                <li value=""> <MdVerified /> Java</li>
+                                <li value=""><MdVerified /> JavaScript</li>
+                                <li value=""><MdVerified /> Python</li>
+                                <li value=""> <MdVerified /> php</li>
                             </ol>
                         </h4>
                     </article>
 
                     <article className='skills__card'>
-                    <  BiCodeAlt className='skills__icons' />
+                        <  BiCodeAlt className='skills__icons' />
                         <h3> Web technology </h3>
                         <h4>
-                            <ol  className='list__techno'>
-                                <li value=""> <MdVerified/> HTML5  CSS3</li>
-                                <li value=""><MdVerified/> Laravel</li>
-                                <li value=""> <MdVerified/> Angular</li>
-                                <li value=""><MdVerified/> Java EE</li>
-                                <li value=""><MdVerified/> React</li>
+                            <ol className='list__techno'>
+                                <li value=""> <MdVerified /> HTML5  CSS3</li>
+                                <li value=""><MdVerified /> Laravel</li>
+                                <li value=""> <MdVerified /> Angular</li>
+                                <li value=""><MdVerified /> Java EE</li>
+                                <li value=""><MdVerified /> React</li>
+                                <li value=""><MdVerified /> Spring-boot</li>
                             </ol>
                         </h4>
                     </article>
 
                     <article className='skills__card'>
-                    <  FiDatabase className='skills__icons' />
+                        <  FiDatabase className='skills__icons' />
                         <h3> Database</h3>
                         <h4>
                             <ol>
-                                <li value=""><MdVerified/> Oracle</li>
-                                <li value=""><MdVerified/> Mysql</li>
-                                <li value=""><MdVerified/> SqlServer</li>
-                                <li value=""><MdVerified/> MongoDB</li>
-                               
+                                <li value=""><MdVerified /> Oracle</li>
+                                <li value=""><MdVerified /> Mysql</li>
+                                <li value=""><MdVerified /> SqlServer</li>
+                                <li value=""><MdVerified /> MongoDB</li>
+
                             </ol>
                         </h4>
                     </article>
 
 
                     <article className='skills__card'>
-                    <  SiDiagramsdotnet className='skills__icons' />
+                        <  SiDiagramsdotnet className='skills__icons' />
                         <h3> Design and Methode</h3>
                         <h4>
                             <ol>
-                                <li value=""><MdVerified/> UML</li>
-                                <li value=""><MdVerified/> Merise</li>
-                                <li value=""><MdVerified/> Design Patterns</li>
-                                <li value=""><MdVerified/> Scrum</li>
-                                <li value=""><MdVerified/>TDD</li>
+                                <li value=""><MdVerified /> UML</li>
+                                <li value=""><MdVerified /> Merise</li>
+                                <li value=""><MdVerified /> Design Patterns</li>
+                                <li value=""><MdVerified /> Scrum</li>
+                                <li value=""><MdVerified />TDD</li>
                             </ol>
                         </h4>
                     </article>
